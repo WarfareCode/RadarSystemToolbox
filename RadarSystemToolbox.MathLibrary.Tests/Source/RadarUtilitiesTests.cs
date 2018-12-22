efcb7e8daed0d27582ddc4cb2f8bb6c2056657df
Brian Tollins <briantollins@outlook.com>
2018-12-22 23:26:04 +0100

    Fixed breaking test.

--- RadarSystemToolbox.MathLibrary.Tests/Source/RadarUtilitiesTests.cs
+++ RadarSystemToolbox.MathLibrary.Tests/Source/RadarUtilitiesTests.cs
@@ -6,6 +6,20 @@ namespace RadarSystemToolbox.MathLibrary.Tests
     public class RadarUtilitiesTests
     {
         [TestMethod]
+        public void CalculateOneWayRangeFromTime_WithValidRange()
+        {
+            // Arrange:
+            var time = 1.0e-6;
+            var expectedResult = 300.0;
+
+            // Act:
+            var result = RadarUtilities.CalculateOneWayRangeFromTime(time);
+
+            // Assert:
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
         public void CalculateTwoWayRangeFromTime_WithValidRange()
         {
             // Arrange:
@@ -14,20 +28,6 @@ namespace RadarSystemToolbox.MathLibrary.Tests
 
             // Act:
             var result = RadarUtilities.CalculateTwoWayRangeFromTime(time);
-
-            // Assert:
-            Assert.AreEqual(expectedResult, result);
-        }
-
-        [TestMethod]
-        public void CalculateOneWayRangeFromTime_WithValidRange()
-        {
-            // Arrange:
-            var time = 1.0e-6;
-            var expectedResult = 150.0;
-
-            // Act:
-            var result = RadarUtilities.CalculateOneWayRangeFromTime(time);
 
             // Assert:
             Assert.AreEqual(expectedResult, result);
