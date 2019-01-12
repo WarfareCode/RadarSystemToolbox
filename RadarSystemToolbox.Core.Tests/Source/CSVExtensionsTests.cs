7bbb6ed2985383dffdb312dd20fb34afbbf84e7d
Brian Tollins <briantollins@outlook.com>
2019-01-12 12:48:08 +0100

    Updated tests.

--- RadarSystemToolbox.Core.Tests/Source/CSVExtensionsTests.cs
+++ RadarSystemToolbox.Core.Tests/Source/CSVExtensionsTests.cs
@@ -13,9 +13,9 @@ namespace RadarSystemToolbox.Core.Tests
         public void WriteToCSVFile_WithRadarRangeEquationInputData()
         {
             // Arrange:
-            var r = new List<RadarRangeEquationInputData> { new RadarRangeEquationInputData(), new RadarRangeEquationInputData(), new RadarRangeEquationInputData() };
+            //var r = new List<RadarRangeEquationInputData> { new RadarRangeEquationInputData(), new RadarRangeEquationInputData(), new RadarRangeEquationInputData() };
 
-            var path = @"C:\temp\RadarRangeEquationInputData.csv";
+            //var path = @"C:\temp\RadarRangeEquationInputData.csv";
 
             // Act:
             //r.WriteToCSVFile(path);
@@ -28,9 +28,9 @@ namespace RadarSystemToolbox.Core.Tests
         public void WriteToCSVFile_WithRadarRangeEquationOutputData()
         {
             // Arrange:
-            var r = new List<RadarRangeEquationOutputData> { new RadarRangeEquationOutputData(), new RadarRangeEquationOutputData(), new RadarRangeEquationOutputData() };
+            //var r = new List<RadarRangeEquationOutputData> { new RadarRangeEquationOutputData(), new RadarRangeEquationOutputData(), new RadarRangeEquationOutputData() };
 
-            var path = @"C:\temp\RadarRangeEquationOutputData.csv";
+            //var path = @"C:\temp\RadarRangeEquationOutputData.csv";
 
             // Act:
             //r.WriteToCSVFile(path);
@@ -43,15 +43,15 @@ namespace RadarSystemToolbox.Core.Tests
         public void WriteToCSVFile_WithRadarRangeEquationData()
         {
             // Arrange:
-            var d1 = new RadarRangeEquationData() { BurstId = 1, Inputs = new RadarRangeEquationInputData(), Outputs = new RadarRangeEquationOutputData() };
+            //var d1 = new RadarRangeEquationData() { BurstId = 1, Inputs = new RadarRangeEquationInputData(), Outputs = new RadarRangeEquationOutputData() };
 
-            var d2 = new RadarRangeEquationData() { BurstId = 2, Inputs = new RadarRangeEquationInputData(), Outputs = new RadarRangeEquationOutputData() };
+            //var d2 = new RadarRangeEquationData() { BurstId = 2, Inputs = new RadarRangeEquationInputData(), Outputs = new RadarRangeEquationOutputData() };
 
-            var d3 = new RadarRangeEquationData() { BurstId = 3, Inputs = new RadarRangeEquationInputData(), Outputs = new RadarRangeEquationOutputData() };
+            //var d3 = new RadarRangeEquationData() { BurstId = 3, Inputs = new RadarRangeEquationInputData(), Outputs = new RadarRangeEquationOutputData() };
 
-            var r = new List<RadarRangeEquationData> { d1, d2, d3 };
+            //var r = new List<RadarRangeEquationData> { d1, d2, d3 };
 
-            var path = @"C:\temp\RadarRangeEquationData.csv";
+            //var path = @"C:\temp\RadarRangeEquationData.csv";
 
             // Act:
             //r.WriteToCSVFile(path);
@@ -64,13 +64,13 @@ namespace RadarSystemToolbox.Core.Tests
         public void WriteToCSVFile_WithComplexData()
         {
             // Arrange:
-            var d1 = new Complex(1, 2);
+            //var d1 = new Complex(1, 2);
 
-            var d2 = new Complex(-1, -2);
+            //var d2 = new Complex(-1, -2);
 
-            var r = new List<Complex> { d1, d2 };
+            //var r = new List<Complex> { d1, d2 };
 
-            var path = @"C:\temp\ComplexData.csv";
+            //var path = @"C:\temp\ComplexData.csv";
 
             // Act:
             //r.WriteToCSVFile(path);
